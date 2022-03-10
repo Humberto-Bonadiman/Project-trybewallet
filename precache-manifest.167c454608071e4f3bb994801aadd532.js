bb28a76b138e99eb115c03464c402abd14a7737f
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-03-10 10:16:39 -0400

    Updates

---
+++ precache-manifest.167c454608071e4f3bb994801aadd532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae8a36e4ca8edc6c9bfa757a23d6cb6",
+    "url": "/Project-trybewallet/index.html"
+  },
+  {
+    "revision": "e19dc7b5f7b77ec6c585",
+    "url": "/Project-trybewallet/static/css/main.70144e4e.chunk.css"
+  },
+  {
+    "revision": "86789b1902f34ba62f10",
+    "url": "/Project-trybewallet/static/js/2.e69ef35f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Project-trybewallet/static/js/2.e69ef35f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19dc7b5f7b77ec6c585",
+    "url": "/Project-trybewallet/static/js/main.10ed9ed6.chunk.js"
+  },
+  {
+    "revision": "e65a59b90d7f958b96c9",
+    "url": "/Project-trybewallet/static/js/runtime-main.8f1244c6.js"
+  }
+]);
